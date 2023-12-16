fc4f651a205667f6dda14351387d084e56d92884
spongebob <374506395@qq.com>
2023-12-16 00:20:51 -0800

    init

---
+++ src/main/java/com/spongebob/socalshopping/SoCalShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.spongebob.socalshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoCalShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoCalShoppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spongebob/socalshopping/SoCalShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spongebob.socalshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoCalShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
