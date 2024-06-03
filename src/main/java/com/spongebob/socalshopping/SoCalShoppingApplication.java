4d87e54e01835469ab568d7148bc51575b4ff6ef
spongebob <374506395@qq.com>
2024-06-02 21:22:08 -0700

    mybatis configuration

--- src/main/java/com/spongebob/socalshopping/SoCalShoppingApplication.java
+++ src/main/java/com/spongebob/socalshopping/SoCalShoppingApplication.java
@@ -1,9 +1,11 @@
 package com.spongebob.socalshopping;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.spongebob.socalshopping.db.mappers")
 public class SoCalShoppingApplication {
 
     public static void main(String[] args) {
