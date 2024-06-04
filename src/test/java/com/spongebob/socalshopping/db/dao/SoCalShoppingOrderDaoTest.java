522ab5751acb0bee2140a12c23e560a81697e323
spongebob <374506395@qq.com>
2024-06-04 01:19:18 -0700

    write unit test for orderDao

---
+++ src/test/java/com/spongebob/socalshopping/db/dao/SoCalShoppingOrderDaoTest.java
@@ -0,0 +1,20 @@
+package com.spongebob.socalshopping.db.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SoCalShoppingOrderDaoTest {
+
+    @Test
+    void insertOrder() {
+    }
+
+    @Test
+    void queryOrderById() {
+    }
+
+    @Test
+    void queryOrderByOrderNo() {
+    }
+}
